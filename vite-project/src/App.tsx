2f84b4980fba9c2f5efd7d8e4bdb27b8fab7b3bf
Andre Smith <andre.frank.smith@gmail.com>
2025-07-17 20:28:48 +0200

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>This is a paragraph</p>
+      <button>Click me</button>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
